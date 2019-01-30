8d9cd7aa08f23c0c8894a8786690b633997a17ca
Myszusz <Michal.Borkowski77@gmail.com>
2019-01-30 16:46:02 +0100

    initial commit
    Whole project commited

---
+++ Assets/Tile.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public GameManager gamemanager;
+    public AudioClip hit;
+    private AudioSource source;
+
+    private void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        gamemanager.AddScore();
+        source.PlayOneShot(hit);
+    }
+}
